bfda947f660aa1336d33bb360610c3c1374fc03d
Sergey Ivanov <ggdlin@gmail.com>
2020-12-21 22:58:02 +0300

    Initial Commit

---
+++ GameHW3-4/GameHW3_4App.swift
@@ -0,0 +1,17 @@
+//
+//  GameHW3_4App.swift
+//  GameHW3-4
+//
+//  Created by Ivanov Sergey on 21.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct GameHW3_4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
