48e4c79f77f3e571ed3d2e7d5d50f8cbe9267c10
Sergey Ivanov <ggdlin@gmail.com>
2020-12-24 00:51:30 +0300

    hw3-4-final-v2

--- GameHW3-4/ContentView.swift
+++ GameHW3-4/ContentView.swift
@@ -8,27 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var currentValue = 50.0
     @State private var targetValue = Int.random(in: 0...100)
-    @State private var score = 0
-    @State private var alert = false
+    @State private var currentValue = 50.0
+    @State private var showAlert = false
+    
     
     var body: some View {
         VStack {
             Text("Передвиньте слайдер, как можно ближе к: \(targetValue)")
             HStack {
                 Text("0")
-                GameUISlider(value: $currentValue, targetValue: $targetValue)
+                GameUISlider(
+                    value: $currentValue,
+                    color: .red,
+                    alphaValue: computeScore())
                 Text("100")
             }
             .padding()
             Button("Проверь меня!") {
-                computeScore()
-                alert = true
+                showAlert = true
             }
             .padding()
-            .alert(isPresented: $alert) {
-                Alert(title: Text("Your Score"), message: Text("\(score)"))
+            .alert(isPresented: $showAlert) {
+                Alert(title: Text("Your Score"), message: Text("\(computeScore())"))
             }
             Button("Начать заново") {
                 targetValue = Int.random(in: 0...100)
@@ -37,15 +39,13 @@ struct ContentView: View {
         
         }
     }
-}
-
-extension ContentView {
     
-    private func computeScore() {
+    private func computeScore() -> Int {
         let difference = abs(targetValue - lround(currentValue))
-        score = 100 - difference
+        return 100 - difference
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
--- GameHW3-4/GameUISlider.swift
+++ GameHW3-4/GameUISlider.swift
@@ -10,14 +10,14 @@ import SwiftUI
 struct GameUISlider: UIViewRepresentable {
     
     @Binding var value: Double
-//    @State var alphaValue: Double = 1
-    @Binding var targetValue: Int
+    let color: UIColor
+    let alphaValue: Int
+
     
     func makeUIView(context: Context) -> UISlider {
         let slider = UISlider(frame: .zero)
         slider.minimumValue = 0
         slider.maximumValue = 100
-        slider.setAlpha(1)
         slider.addTarget(
             context.coordinator,
             action: #selector(Coordinator.valueChanged),
@@ -28,10 +28,16 @@ struct GameUISlider: UIViewRepresentable {
     
     func updateUIView(_ uiView: UISlider, context: Context) {
         uiView.value = Float(value)
+        uiView.thumbTintColor = color.withAlphaComponent(CGFloat(alphaValue) / 100)
+        print("""
+            sender value: \(value)
+            alpha: \(alphaValue)
+            ------------------------
+            """)
     }
     
     func makeCoordinator() -> GameUISlider.Coordinator {
-        Coordinator(value: $value, targetValue: $targetValue)
+        Coordinator(value: $value)
     }
     
     
@@ -40,43 +46,23 @@ struct GameUISlider: UIViewRepresentable {
 extension GameUISlider {
     class Coordinator: NSObject {
         @Binding var value: Double
-        @Binding var targetValue: Int
         
-        init(value: Binding<Double>, targetValue: Binding<Int>) {
+        init(value: Binding<Double>) {
             self._value = value
-            self._targetValue = targetValue
         }
         
         @objc func valueChanged(_ sender: UISlider) {
             value = Double(sender.value)
-            computeAlpha(sender: sender)
             
         }
         
-        private func computeAlpha(sender: UISlider) {
-            let difference = abs(targetValue - lround(Double(sender.value)))
-            let alpha =  Double(100 - difference) / 100
-            print("""
-                target value: \(targetValue)
-                sender value: \(sender.value)
-                difference: \(difference)
-                alpha: \(alpha)
-                ------------------------
-                """)
-            sender.setAlpha(alpha)
-        }
-    }
-}
-
-extension UISlider {
-    func setAlpha(_ alpha: Double) {
-        thumbTintColor = UIColor(red: 1, green: 0, blue: 0, alpha: CGFloat(alpha))
+        
     }
 }
 
 
 struct GameUISlider_Previews: PreviewProvider {
     static var previews: some View {
-        GameUISlider(value: .constant(50.0), targetValue: .constant(10))
+        GameUISlider(value: .constant(50.0), color: .red, alphaValue: 100)
     }
 }
